2f11f37f6a91a32be48f1ba95b4c1cbff4409da0
ephraimg <ephraim.glick@gmail.com>
2017-10-20 17:47:13 -0700

    GET and POST, render app

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -6,3 +6,10 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
   jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
 });
+
+// Server: http://parse.sfs.hackreactor.com/
+// App ID: 42c3ed7c94fed042291633945f78fc083d80d18a
+// API Key: 890ad4e875072efc3ea94f238b1f19d96047e3e6
+
+// Messages endpoint:
+// /chatterbox/classes/messages
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,115 @@
-// YOUR CODE HERE:
+class App {
+
+  constructor(url) {
+    this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
+    this.rooms = ['lobby'];
+    this.storage = [];
+    this.friends = [];
+    // this.username = window.location.search;
+    this.username = 'Ephraim and Rith';
+  }
+
+  // createUsername() {
+  //   this.username = window.location.search;
+  // }
+
+  init() {
+    //this.createUsername();
+    $(document).ready(() => {
+      this.fetch();
+      $('#send').on('submit', e => {
+        e.stopPropagation();
+        e.preventDefault();
+        (this.handleSubmit.bind(this))();
+      });
+      $(document).on('click', '.username', e => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.handleUsernameClick(e.target.textContent);
+      });
+    });
+  }
+
+  send(message) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: message,
+      contentType: 'application/jsonp',
+      success: (data) => {
+        console.log('Success: ', data);
+      },
+      failure: (data) => {
+        console.error(data);
+      }  
+    });
+  }
+
+  fetch() {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      data: {
+        where: {username: "fredx"},
+        //order: -createdAt
+      },
+      success: (data) => {
+        this.storage = data.results;
+        this.storage.forEach(message => this.renderMessage(message));
+        console.log('Success: ', data);
+      },
+      failure: (data) => {
+        console.error(data);
+      }        
+    });
+  }
+
+  handleSubmit() {
+    var post = {};
+    post.username = this.username;
+    // console.log(this.username);
+
+    // console.log('message val: ', $('#message').val());
+
+    post.text = $('#message').val();
+    post.roomname = this.rooms[0];
+    console.log(JSON.stringify(post));
+    this.send(JSON.stringify(post));
+  }
+
+  renderMessage(message) {
+    var name = message.username;
+    var text = message.text;
+    var date = message.createdAt;
+    var $message = $(`<p><span class="username">${name}:</span><br>
+                      ${text} <br> <span class="date">${date}</span></p>`);
+    $('#chats').prepend($message);
+  }
+
+  clearMessages() {
+    $('#chats').empty();
+  }
+
+  addRoom(roomName) {
+    this.rooms.push(roomName);
+    this.renderRoom(roomName);
+  }
+
+  renderRoom(roomName) {
+    var $room = $(`<option value=${roomName}>${roomName}</option>`);
+    $('#roomSelect').prepend($room);
+  }
+
+  renderAllRooms() {
+    this.rooms.forEach(room => renderRoom(room));
+  }
+
+  handleUsernameClick(name) {
+    this.friends.push(name);
+  }
+
+
+}
+
+var app = new App();
+app.init();
